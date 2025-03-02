19f3d0cc42e0da886e8ea78719181e501626b761
Marco Massenzio <m@massenz.io>
2025-03-01 22:19:45 -0700

    Cleanup

--- main.cpp
+++ main.cpp
@@ -2,14 +2,10 @@
 
 import demo;
 
-int f();
-void g(double, int = f() );
-
-
 int main() {
   auto lang = "C++";
   auto s = std::format("Hello and welcome to {}", lang);
-  demo::emit(s);
+  demo::emit(std::format("Hello and welcome to {}", lang));
 
   for (int i = 1; i <= 5; i++) {
     demo::emit(std::format("i = {}", i));
